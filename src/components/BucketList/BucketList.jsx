0cb95be765f5b98e22d1adfd3ceffde1fc6838ce
InnaMcGrane <innamcgrane@gmail.com>
2024-11-25 08:03:20 +0000

    memo get Categories func,highlight active filter category

--- src/components/BucketList/BucketList.jsx
+++ src/components/BucketList/BucketList.jsx
@@ -1,19 +1,20 @@
 import Filter from "../Filter/Filter";
 import cn from "classnames";
 import styles from "./bucketList.module.scss";
-import data from '../../data.js';
+import data from "../../data.js";
 import Task from "../Task/Task.jsx";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 
 function BucketList() {
   const [tasks, setTasks] = useState([]);
-  const [curruntFilterCategory, setCurruntFilterCategory] = useState('All')
+  const [curruntFilterCategory, setCurruntFilterCategory] = useState("All");
 
   useEffect(() => {
-    setTasks([...data])
-  }, [])
-  
-    const getCategories = () => {
+    setTasks([...data]);
+  }, []);
+
+  const getCategories = () => {
+    console.log('call getCategories func');
     const arr = tasks.reduce(
       (acc, el) => {
         return [...acc, el.category];
@@ -21,20 +22,20 @@ function BucketList() {
       ["All"]
     );
     return Array.from(new Set(arr));
-  }
+  };
+
+  const memoCategories = useMemo(() => getCategories(), [tasks]);
 
   return (
     <div className={cn(styles["bucket-list"])}>
       <main className={cn(styles["bucket-list__main"])}>
         <div className={cn(styles["bucket-list__filter"])}>
-          <Filter categories={getCategories()}/>
+          <Filter categories={memoCategories} curruntFilterCategory={curruntFilterCategory} />
         </div>
         <div className={cn(styles["bucket-list__content"])}>
-          {
-            tasks.map((el) => {
-            return <Task title={el.title} desc={el.desc} key={el.id}/>
-            })
-          }
+          {tasks.map((el) => {
+            return <Task title={el.title} desc={el.desc} key={el.id} />;
+          })}
         </div>
       </main>
     </div>
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,12 +2,12 @@ import FilterItem from "../FilterItem/FilterItem";
 import cn from "classnames";
 import styles from "./filter.module.scss";
 
-function Filter({categories}) {
+function Filter({categories, curruntFilterCategory}) {
   return (
     <div className={cn(styles["filter"])}>
      {
         categories.map((category, i) => {
-            return <FilterItem text={category} key={i}/>
+            return <FilterItem text={category} key={i} active={curruntFilterCategory === category ? true : false} />;
         })
      }
     </div>
