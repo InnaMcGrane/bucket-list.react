711d8c0cbb85a79bfb0142745c6b8229cf701ca2
InnaMcGrane <innamcgrane@gmail.com>
2024-12-19 08:05:21 +0000

    got data from TaskForm

--- src/components/CreateTaskForm/CreateTaskForm.jsx
+++ src/components/CreateTaskForm/CreateTaskForm.jsx
@@ -3,11 +3,43 @@ import styles from "./createTaskForm.module.scss";
 import cn from "classnames";
 
 function CreateTaskForm() {
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const formObj = new FormData(event.target);
+
+    const arrPairs = Array.from(formObj);
+
+    // [['title', 'any title'], ['description', 'any description'], ...]
+
+    // {
+    //   title: 'any title',
+    //   description: 'any description'
+    // }
+
+    const taskData =arrPairs.reduce((acc, pair) => {
+      acc[pair[0]] = pair[1];
+      return acc;
+    }, {});
+    console.log(taskData);
+  };
+
+  /* 
+  
+  форма
+  1.  жмем на кнопку Add Task
+  2. onSubmit
+  3. submitHandler
+  4. event
+  5. создаем объект от класса FormData чтобы поучить все данные разом (в FormData передаем ссылку на форму)
+  6. данные у нас formObj
+  
+  */
+
   return (
-    <form className={cn(styles["create-task-form"])}>
-      <input className={cn(styles["create-task-form__input"])} placeholder="title"></input>
-      <input className={cn(styles["create-task-form__input"])} placeholder="description"></input>
-      <input className={cn(styles["create-task-form__input"])} placeholder="category"></input>
+    <form className={cn(styles["create-task-form"])} method="post" action="#" onSubmit={submitHandler}>
+      <input className={cn(styles["create-task-form__input"])} placeholder="title" name="title"></input>
+      <input className={cn(styles["create-task-form__input"])} placeholder="description" name="description"></input>
+      <input className={cn(styles["create-task-form__input"])} placeholder="category" name="category"></input>
       <Button text="Add Task" type="primary" handler={() => {}} />
     </form>
   );
